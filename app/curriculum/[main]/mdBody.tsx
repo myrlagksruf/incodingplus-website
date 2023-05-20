45a3ab42d1417170c5acdc891125594e5b68a229
Seongjun Mun <colonist4@gmail.com>
2023-05-20 21:31:48 +0900

    커리큘럼 페이지 md 파일 기반으로 변경

---
+++ app/curriculum/[main]/mdBody.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { FC, useLayoutEffect, useState } from "react";
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from "rehype-raw";
+import remarkGfm from 'remark-gfm';
+
+export const MDBody:FC<{ mdUrl: string }> = ({ mdUrl }) => {
+    const [markdown, setMarkdown] = useState('');
+
+    useLayoutEffect(() => {
+        const getMarkdown = async () => {
+            const res = await fetch(mdUrl);
+            const text = await res.text();
+            setMarkdown(text);
+        }
+        getMarkdown();
+    }, [mdUrl]);
+
+    return <ReactMarkdown
+      rehypePlugins={[rehypeRaw]}
+      remarkPlugins={[remarkGfm]}
+      components={{
+          a({node, className, children, href, ...props}){
+              const hrefObj:{href?:string} = {};
+              if(href){
+                  const result = new URL(href, mdUrl);
+                  hrefObj.href = result.href;
+              }
+              return <a {...props} className={className} {...hrefObj}>{href}{children}</a>
+          },
+          img({node, className, src, ...props}){
+              const srcObj:{src?:string} = {};
+              if(src){
+                  const result = new URL(src, mdUrl);
+                  srcObj.src = result.href;
+              }
+              return <img {...props} className={className} {...srcObj} />
+          }
+      }}
+      children={markdown}
+  />
+}
